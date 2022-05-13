0b477da3efc037ec3a29af374b6fc350d39b3b19
Aleksandr-Glebik <glebikaa@mail.ru>
2022-05-13 19:22:12 +0300

    feat: add markup, style, img and objText in js (Fri, May 13, 2022 07:22:12 PM)

---
+++ cssMemSlider/index.js
@@ -0,0 +1,7 @@
+const objStatements = {
+    carrer: 'Career growth in the company when you have no family ties',
+    ofer: 'When received the first offer',
+    relacate: 'When you have a few days to relacate',
+    sideJob: 'My source of income while studying frontend',
+    understand: 'When realized the difference between var and let'
+}
